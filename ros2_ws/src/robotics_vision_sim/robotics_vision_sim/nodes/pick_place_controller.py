ee002b297fc73341c1fe265cbe4bf9971aefa78b
MiFanTx <mifantx@hotmail.com>
2026-02-26 16:04:40 +0800

    Task 10.6 complete: pick_place_controller with MoveIt2 integration, all stages executing with wait_until_executed

--- ros2_ws/src/robotics_vision_sim/robotics_vision_sim/nodes/pick_place_controller.py
+++ ros2_ws/src/robotics_vision_sim/robotics_vision_sim/nodes/pick_place_controller.py
@@ -7,12 +7,17 @@ from rclpy.action.server import ServerGoalHandle
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 
+from pymoveit2 import MoveIt2
+from pymoveit2.robots import ur as robot
+
 from robotics_vision_sim_msgs.action import PickPlace
 
 class PickPlaceController(Node):
     
     def __init__(self):
         super().__init__('pick_place_server')
+        
+        self.safe_height = 0.3
 
         self.callback_group = ReentrantCallbackGroup()
 
@@ -25,6 +30,19 @@ class PickPlaceController(Node):
             cancel_callback=self.cancel_callback,
             callback_group=self.callback_group
         )
+
+        self.moveit2 = MoveIt2(
+            node=self,
+            joint_names=robot.joint_names(),
+            base_link_name=robot.base_link_name(),
+            end_effector_name=robot.end_effector_name(),
+            group_name=robot.MOVE_GROUP_ARM,
+            callback_group=self.callback_group,
+            use_move_group_action=True
+        )
+
+        self.moveit2.max_velocity = 0.5
+        self.moveit2.max_acceleration = 0.5
 
         self.get_logger().info('Pick and Place Controller Initialised!')
 
@@ -42,19 +60,28 @@ class PickPlaceController(Node):
 
         stages = [
             ('MOVING_TO_OBJECT', 20.0, 'Moving to pick position...'),
-            ('LOWERING', 30.0, 'Lowering to object...'),
+            ('LOWERING_TO_OBJECT', 30.0, 'Lowering to object...'),
             ('GRASPING', 40.0, 'Closing gripper...'),
             ('LIFTING', 50.0, 'Lifting object...'),
             ('MOVING_TO_TARGET', 60.0, 'Moving to place position...'),
-            ('LOWERING', 70.0, 'Lowering to target...'),            
+            ('LOWERING_TO_TARGET', 70.0, 'Lowering to target...'),            
             ('PLACING', 80.0, 'Opening gripper...'),
-            ('RETREATING', 100.0, 'Retreating to safe position...'),
+            ('RETREATING', 90.0, 'Retreating to safe position...'),
+            ('HOMING', 100.0, 'Backing to home...'),
         ]
 
         start_time = time.time()
 
+        obj_pos = goal_handle.request.object_pose.pose.position
+        obj_ori = goal_handle.request.object_pose.pose.orientation
+        tgt_pos = goal_handle.request.target_pose.pose.position
+        tgt_ori = goal_handle.request.target_pose.pose.orientation
+
+        obj_safe_z = obj_pos.z + self.safe_height
+        tgt_safe_z = tgt_pos.z + self.safe_height
+
         for stage_name, progress, status_msgs in stages:
-            
+
             if goal_handle.is_cancel_requested:
                 goal_handle.canceled()
 
@@ -65,14 +92,62 @@ class PickPlaceController(Node):
 
                 self.get_logger().info(f'Goal canceled during {stage_name}')
                 return result
-
+            
             feedback.current_stage = stage_name
             feedback.progress_percent = progress
             feedback.status_message = status_msgs
 
             goal_handle.publish_feedback(feedback)
-
             self.get_logger().info(f'  [{progress:.0f}%] {stage_name}: {status_msgs}')
+
+            if stage_name == 'MOVING_TO_OBJECT':
+                self.moveit2.move_to_pose(
+                    position=[obj_pos.x, obj_pos.y, obj_safe_z],
+                    quat_xyzw=[obj_ori.x, obj_ori.y, obj_ori.z, obj_ori.w]
+                )
+
+            elif stage_name == 'LOWERING_TO_OBJECT':
+                self.moveit2.move_to_pose(
+                    position=obj_pos,
+                    quat_xyzw=obj_ori
+                )
+            
+            elif stage_name == 'GRASPING':
+                pass
+
+            elif stage_name == 'LIFTING':
+                self.moveit2.move_to_pose(
+                    position=[obj_pos.x, obj_pos.y, obj_safe_z],
+                    quat_xyzw=[obj_ori.x, obj_ori.y, obj_ori.z, obj_ori.w]
+                )
+
+            elif stage_name == 'MOVING_TO_TARGET':
+                self.moveit2.move_to_pose(
+                    position=[tgt_pos.x, tgt_pos.y, tgt_safe_z],
+                    quat_xyzw=[tgt_ori.x, tgt_ori.y, tgt_ori.z, tgt_ori.w]
+                )
+            
+            elif stage_name == 'LOWERING_TO_TARGET':
+                self.moveit2.move_to_pose(
+                    position=tgt_pos, 
+                    quat_xyzw=tgt_ori
+                )
+            
+            elif stage_name == 'PLACING':
+                pass
+
+            elif stage_name == 'RETREATING':
+                self.moveit2.move_to_pose(
+                    position=[tgt_pos.x, tgt_pos.y, tgt_safe_z],
+                    quat_xyzw=[tgt_ori.x, tgt_ori.y, tgt_ori.z, tgt_ori.w]
+                )
+
+            elif stage_name == 'HOMING':
+                self.moveit2.move_to_configuration(
+                    joint_positions=[0.0, -1.5707, 0.0, -1.5707, 0.0, 0.0]
+                ) # move back home
+
+            self.moveit2.wait_until_executed()
 
         result = PickPlace.Result()
         result.status = PickPlace.Result.STATUS_SUCCESS
